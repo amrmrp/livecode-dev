e725003221abe3402290fe19c8ab7519619665cd
amrmrp <amirreza.moghaddampoor@gmail.com>
2026-02-25 22:03:25 +0330

    feat(sitemap): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://livecode-dev.ir',
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {     
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      wrap: true,
+    },
+  },
+});
